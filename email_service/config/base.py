d32772dfe7ab09cdf6b3c3b95e1fdd274ac33a1e
Tapan Pandita <tapan.pandita@gmail.com>
2014-09-09 22:35:23 -0700

    linting fixes

--- email_service/config/base.py
+++ email_service/config/base.py
@@ -2,19 +2,20 @@
 import os
 
 
-#### FLASK CONFIG
+# FLASK CONFIG
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-#### EMAIL CONFIG
+# EMAIL CONFIG
 DEFAULT_FROM_EMAIL = 'support@tapandita.com'
 DEFAULT_FROM_NAME = 'Tapan Pandita'
+EMAIL_DOMAIN = 'tapandita.com'
 
-#### SENDGRID CONFIG
+# SENDGRID CONFIG
 SENDGRID_HOST = 'https://api.sendgrid.com'
 SENDGRID_USER = os.environ.get('SENDGRID_USER')
 SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 
-#### MAILGUN CONFIG
+# MAILGUN CONFIG
 MAILGUN_HOST = 'https://api.mailgun.net'
 MAILGUN_USER = os.environ.get('MAILGUN_USER')
 MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY')
--- email_service/config/testing.py
+++ email_service/config/testing.py
@@ -1,20 +1,20 @@
 '''Test configuration'''
 
-#### FLASK CONFIG
+# FLASK CONFIG
 TESTING = True
 DEBUG = True
 SECRET_KEY = 'test_secret_key'
 
-#### EMAIL CONFIG
+# EMAIL CONFIG
 DEFAULT_FROM_EMAIL = 'support@tapandita.com'
 DEFAULT_FROM_NAME = 'Tapan Pandita'
 
-#### SENDGRID CONFIG
+# SENDGRID CONFIG
 SENDGRID_HOST = 'https://api.sendgrid.com'
 SENDGRID_USER = 'user'
 SENDGRID_API_KEY = 'hd23ofh28239chf2'
 
-#### MAILGUN CONFIG
+# MAILGUN CONFIG
 MAILGUN_HOST = 'https://api.mailgun.net'
 MAILGUN_USER = 'api'
 MAILGUN_API_KEY = 'f23honeo9p0uj20'
