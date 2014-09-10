4938f5e96322b02a5b4b5a3576d130a553465eba
Tapan Pandita <tapan.pandita@gmail.com>
2014-09-09 22:37:05 -0700

    added an init method to exceptions

--- email_service/mail/exceptions.py
+++ email_service/mail/exceptions.py
@@ -3,10 +3,14 @@
 class BaseEmailException(Exception):
     '''Base class for exceptions related to sending email'''
 
+    def __init__(self, status_code, error_message):
+        self.status_code = status_code
+        self.error_message = error_message
+
 
 class ClientException(BaseEmailException):
-    '''Error in data supplied to API, 4xx error'''
+    '''Error in data supplied to API, typically 4xx error'''
 
 
 class ServerException(BaseEmailException):
-    '''Error at remote server, 5xx error'''
+    '''Error at remote server, typically 5xx error'''
